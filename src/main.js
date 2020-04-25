1361b4e38ce786a067e8daf166e734b14cc76b82
Koen van Gilst <koen.vangilst@gmail.com>
2020-04-25 20:29:37 +0200

    WIP - adds serviceworker

--- src/main.js
+++ src/main.js
@@ -6,4 +6,26 @@ const app = new App({
   target: document.body,
 });
 
+const production = process.env.NODE_ENV === "production";
+// Check that service workers are supported
+// only load in production
+if ("serviceWorker" in navigator) {
+  console.log("prod", production, process.env.NODE_ENV);
+  // Use the window load event to keep the page load performant
+  window.addEventListener("load", () => {
+    // only load in production
+    if (production) navigator.serviceWorker.register("serviceworker.js");
+
+    // remove all old service workers when developing
+    if (!production) {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        console.log("Unloading service workers", registrations);
+        for (let registration of registrations) {
+          registration.unregister();
+        }
+      });
+    }
+  });
+}
+
 export default app;
