fd456441bf90c29a1791041dfb4816c0284e8812
Koen van Gilst <koen.vangilst@gmail.com>
2020-04-27 17:51:58 +0200

    fix - keep message text inside div on safari

--- src/Message.svelte
+++ src/Message.svelte
@@ -102,8 +102,28 @@
     padding: 0.4em 1em;
     background-color: #eee;
     border-radius: 1em 1em 1em 0;
-    hyphens: auto;
+
+    /* This makes sure returns are also rendered */
     white-space: pre-wrap;
+
+    /* The trouble you have to go through to keep simple text inside it's div! 😆 */
+    /* Source: https://css-tricks.com/snippets/css/prevent-long-urls-from-breaking-out-of-container/ */
+
+    /* These are technically the same, but use both */
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+
+    -ms-word-break: break-all;
+    /* This is the dangerous one in WebKit, as it breaks things wherever */
+    word-break: break-all;
+    /* Instead use this non-standard one: */
+    word-break: break-word;
+
+    /* Adds a hyphen where the word breaks, if supported (No Blink) */
+    -ms-hyphens: auto;
+    -moz-hyphens: auto;
+    -webkit-hyphens: auto;
+    hyphens: auto;
   }
 
   .user {
