40bfa07c3e400c5e866c7eb673645c66a9c57144
Koen van Gilst <koen@koenvangilst.nl>
2023-04-01 14:28:35 +0200

    fix: update servers

--- src/initGun.js
+++ src/initGun.js
@@ -8,12 +8,11 @@ if (process.env.NODE_ENV === "development") {
 } else {
   peers = [
     // Community relay peers: https://github.com/amark/gun/wiki/volunteer.dht
-    "https://www.raygun.live/gun",
-    "https://gunmeetingserver.herokuapp.com/gun",
-    "https://gun-us.herokuapp.com/gun",
-    "https://gun-eu.herokuapp.com/gun",
-    // My own relay peer
-    // "https://phrassed.com/gun",
+    "https://gun-manhattan.herokuapp.com/gun",
+    "https://relay.129.153.59.37.nip.io/gun",
+    "https://relay.peer.ooo/gun",
+    "https://peer.wallie.io/gun",
+    "www-dweb-gun.dev.archive.org/gun",
   ];
 }
 
